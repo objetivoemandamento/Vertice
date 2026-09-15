53740207102322ae4c19ee96ae88ee510f9cdafa
objetivoemandamento <najelamauricio@gmail.com>
2026-09-15 19:26:39 -0300

    Bump Android release to 1.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vertice.launcher"
         minSdk = 23
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.4.3"
+        versionCode = 18
+        versionName = "1.4.4"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
