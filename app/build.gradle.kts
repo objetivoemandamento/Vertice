5b3d91df78ce5d6a17817b6c5450e54d6a5a5485
objetivoemandamento <najelamauricio@gmail.com>
2026-09-15 01:01:34 -0300

    Bump VERTICE Android version to 1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,18 @@ android {
         applicationId = "com.vertice.launcher"
         minSdk = 23
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.4.0"
+        versionCode = 15
+        versionName = "1.4.1"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
     }
 }
 
