1904a9322b07974f5451a396694a0b6c6caa7d37
objetivoemandamento <najelamauricio@gmail.com>
2026-09-17 20:23:14 -0300

    feat(gsi): add AOSP product integration

---
+++ rom/gsi/AndroidProducts.mk
@@ -0,0 +1,8 @@
+# VERTICE GSI integration manifest.
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/vertice_gsi.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_arm64_ab-userdebug \
+    aosp_arm_64b_ab-userdebug \
+    aosp_arm_64b_a-userdebug
