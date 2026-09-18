5b89d19bf2fd5cfdd1197406ab031cea00173528
objetivoemandamento <najelamauricio@gmail.com>
2026-09-18 20:47:32 -0300

    feat: add five international marketing markets

---
+++ server/src/markets.js
@@ -0,0 +1,9 @@
+const VERTICE_MARKETS = [
+  { countryCode: 'BR', countryName: 'Brasil', locale: 'pt-BR', language: 'Português (Brasil)', currency: 'BRL', symbol: 'R$' },
+  { countryCode: 'US', countryName: 'Estados Unidos', locale: 'en-US', language: 'English (US)', currency: 'USD', symbol: '$' },
+  { countryCode: 'PT', countryName: 'Portugal', locale: 'pt-PT', language: 'Português (Portugal)', currency: 'EUR', symbol: '€' },
+  { countryCode: 'ES', countryName: 'Espanha', locale: 'es-ES', language: 'Español (España)', currency: 'EUR', symbol: '€' },
+  { countryCode: 'MX', countryName: 'México', locale: 'es-MX', language: 'Español (México)', currency: 'MXN', symbol: 'MX$' }
+];
+
+module.exports = { VERTICE_MARKETS };
