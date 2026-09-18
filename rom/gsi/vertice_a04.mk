39079b77bfbbb8c342512a8bf69aafa499f4c66d
objetivoemandamento <najelamauricio@gmail.com>
2026-09-18 18:42:27 -0300

    fix(gsi): use explicit VERTICE product include path

--- rom/gsi/vertice_a04.mk
+++ rom/gsi/vertice_a04.mk
@@ -2,7 +2,7 @@
 # Base: Android 12 GSI, ARM64 userspace / Binder64 / system-as-root.
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_arm64.mk)
-$(call inherit-product, $(LOCAL_DIR)/vertice_gsi.mk)
+$(call inherit-product, device/vertice/gsi/vertice_gsi.mk)
 
 PRODUCT_NAME := vertice_a04
 PRODUCT_DEVICE := vertice_a04
