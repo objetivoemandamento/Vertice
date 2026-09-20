22a5ac2078d677ca53942e580b157c699ea9f0ce
objetivoemandamento <najelamauricio@gmail.com>
2026-09-20 14:16:44 -0300

    fix: define currencyCode for checkout markets

--- server/src/markets.js
+++ server/src/markets.js
@@ -1,9 +1,9 @@
 const VERTICE_MARKETS = [
-  { countryCode: 'BR', countryName: 'Brasil', locale: 'pt-BR', language: 'Português (Brasil)', currency: 'BRL', symbol: 'R$' },
-  { countryCode: 'US', countryName: 'Estados Unidos', locale: 'en-US', language: 'English (US)', currency: 'USD', symbol: '$' },
-  { countryCode: 'PT', countryName: 'Portugal', locale: 'pt-PT', language: 'Português (Portugal)', currency: 'EUR', symbol: '€' },
-  { countryCode: 'ES', countryName: 'Espanha', locale: 'es-ES', language: 'Español (España)', currency: 'EUR', symbol: '€' },
-  { countryCode: 'MX', countryName: 'México', locale: 'es-MX', language: 'Español (México)', currency: 'MXN', symbol: 'MX$' }
+  { countryCode: 'BR', countryName: 'Brasil', locale: 'pt-BR', language: 'Português (Brasil)', currency: 'BRL', currencyCode: 'BRL', symbol: 'R$' },
+  { countryCode: 'US', countryName: 'Estados Unidos', locale: 'en-US', language: 'English (US)', currency: 'USD', currencyCode: 'USD', symbol: '$' },
+  { countryCode: 'PT', countryName: 'Portugal', locale: 'pt-PT', language: 'Português (Portugal)', currency: 'EUR', currencyCode: 'EUR', symbol: '€' },
+  { countryCode: 'ES', countryName: 'Espanha', locale: 'es-ES', language: 'Español (España)', currency: 'EUR', currencyCode: 'EUR', symbol: '€' },
+  { countryCode: 'MX', countryName: 'México', locale: 'es-MX', language: 'Español (México)', currency: 'MXN', currencyCode: 'MXN', symbol: 'MX$' }
 ];
 
 module.exports = { VERTICE_MARKETS };
