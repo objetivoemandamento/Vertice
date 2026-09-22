04a46ce19fc17dbe9c6c5463cde903f6eb913bea
objetivoemandamento <najelamauricio@gmail.com>
2026-09-22 09:38:44 -0300

    feat: run durable outbox publisher with worker

--- server/src/worker.ts
+++ server/src/worker.ts
@@ -1,12 +1,6 @@
 import "./queue/executionWorker";
+import { publishPendingOutbox } from "./outbox/publisher";
 console.log("[vertice-worker] execution worker started");
-process.on("SIGTERM", async () => {
-  const { executionWorker } = await import("./queue/executionWorker");
-  await executionWorker.close();
-  process.exit(0);
-});
-process.on("SIGINT", async () => {
-  const { executionWorker } = await import("./queue/executionWorker");
-  await executionWorker.close();
-  process.exit(0);
-});
+const timer=setInterval(()=>publishPendingOutbox(100).catch(error=>console.error("[vertice-outbox]",error instanceof Error?error.message:"publish failed")),1000);
+const shutdown=async(signal:string)=>{clearInterval(timer);console.log("[vertice-worker] "+signal);const {executionWorker}=await import("./queue/executionWorker");await executionWorker.close();process.exit(0);};
+process.once("SIGTERM",()=>shutdown("SIGTERM"));process.once("SIGINT",()=>shutdown("SIGINT"));
