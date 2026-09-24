def681d2927bd3f5e82e443162b8abb7edc931a7
objetivoemandamento <najelamauricio@gmail.com>
2026-09-24 07:00:59 -0300

    fix: grant RLS membership function only to app roles

--- server/migrations/20260924023000_private_rls_function.sql
+++ server/migrations/20260924023000_private_rls_function.sql
@@ -14,7 +14,15 @@ as $$
   )
 $$;
 
+grant usage on schema private to authenticated;
 grant execute on function private.app_is_tenant_member(uuid) to authenticated;
+do $grant$
+begin
+  if exists (select 1 from pg_roles where rolname='vertice_app') then
+    grant usage on schema private to vertice_app;
+    grant execute on function private.app_is_tenant_member(uuid) to vertice_app;
+  end if;
+end $grant$;
 revoke execute on function private.app_is_tenant_member(uuid) from anon,public;
 
 do $vertice$
